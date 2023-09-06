47afa86a165156a9c13d769deae99de737369ce0
molo06002 <molo06002@E-PW0234KQ>
2023-09-06 12:38:30 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace uppgift_1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Du är nästan klar med kapitel 1.");
+            Console.WriteLine("Du har lärt dig skriva ut tecken som \" och \"\\");
+        }
+    }
+}
